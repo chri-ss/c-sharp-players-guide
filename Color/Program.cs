ba41a0d74f97847a4c0699b29dc299ff7957054b
chri-ss <christorper@gmail.com>
2023-03-25 00:40:17 -0700

    Add Color class

---
+++ Color/Program.cs
@@ -0,0 +1,18 @@
+﻿class Color
+{
+    int min = 0;
+    int max = 255;
+
+    public int Red { get; private set; }
+    public int Green { get; private set; }
+    public int Blue { get; private set; }
+
+    public (int r, int g, int b) rbg;
+
+    public Color(int r, int g, int b)
+    {
+        Red = r;
+        Green = g;
+        Blue = b;
+    }
+}
