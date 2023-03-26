578c9cdea1c573aff622ffe944206955505b90a9
chri-ss <christorper@gmail.com>
2023-03-26 12:55:59 -0700

    Add Door class and methods for opening/closing

---
+++ Chapter-24/Locked-Door/Program.cs
@@ -0,0 +1,65 @@
+﻿Door door = new Door(1234);
+
+door.Open(1234);
+door.Close(1234);
+
+class Door
+{
+    private int Passcode { get; set; }
+
+    private DoorStates OpenState { get; set; }
+    private DoorStates LockState { get; set; }
+
+    public void Open(int pass)
+    {
+        if (pass == Passcode)
+        {
+            if (OpenState == DoorStates.Closed && LockState == DoorStates.Unlocked)
+            {
+                OpenState = DoorStates.Open;
+                LogDoorState();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Wrong Pass Code.");
+        }
+    }
+    public void Close(int pass)
+    {
+        if (pass == Passcode)
+        {
+            if (OpenState == DoorStates.Open)
+            {
+                OpenState = DoorStates.Closed;
+                LogDoorState();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Wrong Pass Code.");
+        }
+    }
+    public void Lock(int pass) 
+    {
+        
+    }
+    public void Unlock(int pass) 
+    {
+        
+    }
+
+    public void LogDoorState()
+    {
+        Console.WriteLine($"The Door is {OpenState}");
+    }
+
+    public Door(int passcode)
+    {
+        Passcode = passcode;
+        OpenState = DoorStates.Closed;
+        LockState = DoorStates.Unlocked;
+    }
+
+    enum DoorStates { Open, Closed, Locked, Unlocked }
+}
