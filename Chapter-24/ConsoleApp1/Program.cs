875ebf8bcf20f262c3dea01c4e24b1170057dbdf
chri-ss <christorper@gmail.com>
2023-03-29 22:07:15 -0700

    Validate password lengths

---
+++ Chapter-24/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿PasswordValidator pv = new PasswordValidator();
+
+pv.Validate("nope");
+pv.Validate("amsterdam");
+pv.Validate("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+class PasswordValidator
+{
+    private int min = 6;
+    private int max = 13;
+    public PasswordValidator()
+    {
+
+    }
+
+    public void Validate(string pass)
+    {
+        if (pass.Length >= min && pass.Length <= max)
+        {
+            Console.WriteLine("Valid password");
+        }
+        else
+        {
+            Console.WriteLine("Invalid password");
+        }
+    }
+}
