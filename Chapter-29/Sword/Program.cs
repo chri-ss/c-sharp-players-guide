d7f82ff0fec9458ec49e73457803be8ecb4c88d8
chri-ss <christorper@gmail.com>
2023-04-09 12:31:44 -0700

    Define enums for sword material and gemstones and a record for sword

---
+++ Chapter-29/Sword/Program.cs
@@ -0,0 +1,4 @@
+﻿public record Sword(SwordMaterial material, Gemstones gemstone, int length, int width);
+
+public enum SwordMaterial { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gemstones { Emerald, Amber, Sapphire, Diamond, Bitstone }
