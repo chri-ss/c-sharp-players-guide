3dc5c4bd7369f2abdad6193f9d4e3b5ac010eae3
chri-ss <christorper@gmail.com>
2023-04-09 12:39:02 -0700

    Create some test swords

--- Chapter-29/Sword/Program.cs
+++ Chapter-29/Sword/Program.cs
@@ -1,4 +1,14 @@
-﻿public record Sword(SwordMaterial material, Gemstones gemstone, int length, int width);
+﻿Sword sword1 = new Sword(SwordMaterial.Iron, Gemstones.None, 16, 4);
+
+Sword sword2 = sword1 with { material = SwordMaterial.Steel };
+
+Sword sword3 = sword2 with { gemstone = Gemstones.Sapphire };
+
+Sword sword4 = sword2 with { gemstone = Gemstones.Diamond, length = 20 };
+
+Console.WriteLine($"{sword1.ToString()}, {sword2.ToString()}, {sword3.ToString()}, {sword4.ToString()}");
+
+public record Sword(SwordMaterial material, Gemstones gemstone, int length, int width);
 
 public enum SwordMaterial { Wood, Bronze, Iron, Steel, Binarium }
-public enum Gemstones { Emerald, Amber, Sapphire, Diamond, Bitstone }
+public enum Gemstones { Emerald, Amber, Sapphire, Diamond, Bitstone, None }
