783a4ea5c5549017634a65d9c5297ed7a2e4d734
chri-ss <christorper@gmail.com>
2023-04-12 21:42:10 -0700

    Add a class for changing the color of an item

---
+++ Chapter-30/ColoredItem/Program.cs
@@ -0,0 +1,41 @@
+﻿ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+sword.Display();
+bow.Display();
+axe.Display(); 
+
+public class Sword
+{
+
+}
+
+public class Bow
+{
+
+}
+
+public class Axe
+{
+
+}
+
+public class ColoredItem<T>
+{
+    public T item { get; private set; }
+
+    public ConsoleColor itemColor { get; private set; }
+
+    public ColoredItem(T item, ConsoleColor itemColor)
+    {
+        this.item = item;
+        this.itemColor = itemColor;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = itemColor;
+        Console.WriteLine(item?.ToString());
+    }
+}
