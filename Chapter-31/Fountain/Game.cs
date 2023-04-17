277b304fa69743875d45c1a4aa91ea55319074a2
chri-ss <christorper@gmail.com>
2023-04-16 19:58:24 -0700

    Add method for moving around the map

--- Chapter-31/Fountain/Game.cs
+++ Chapter-31/Fountain/Game.cs
@@ -4,6 +4,67 @@ namespace Fountain
 {
     internal class Game
     {
-        public Map map = new Map(4, 4);
+        public Map map;
+        public Tile current { get; private set; }
+
+        public Game()
+        {
+            map = new Map(4, 4);
+            current = map.TileMap[0, 0];
+        }
+
+        public void Move(int direction)
+        {
+            switch (direction)
+            {
+                case 0:
+                    // move north
+                    if (current.X > 0 && current.X <= 3)
+                    {
+                        current = map.TileMap[current.X - 1, current.Y];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid movement");
+                    }
+                    break;
+
+                case 1:
+                    // move right
+                    if (current.Y >= 0 && current.Y < 3)
+                    {
+                        current = map.TileMap[current.X, current.Y + 1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid movement");
+                    }
+                    break;
+
+                case 2:
+                    // move south
+                    if (current.X >= 0 && current.X < 3)
+                    {
+                        current = map.TileMap[current.X + 1, current.Y];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid movement");
+                    }
+                    break;
+
+                case 3:
+                    // move left
+                    if (current.Y > 0 && current.Y <= 3)
+                    {
+                        current = map.TileMap[current.X, current.Y - 1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid movement");
+                    }
+                    break;
+            }
+        }
     }
 }
--- Chapter-31/Fountain/Program.cs
+++ Chapter-31/Fountain/Program.cs
@@ -2,4 +2,11 @@
 
 Game game = new Game();
 
-game.map.DisplayMap();
+while (true)
+{
+    Console.WriteLine("Enter a movement direction");
+    int direction = Convert.ToInt32(Console.ReadLine());
+
+    game.Move(direction);
+    Console.WriteLine($"({game.current.X}, {game.current.Y})");
+}
