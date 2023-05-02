f8de5206c664296a7182aa62dfc16e743c727dbb
chri-ss <christorper@gmail.com>
2023-05-01 19:20:04 -0700

    Simple number and bool parse

---
+++ Chapter-34/NumberParser/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.WriteLine("Enter a number");
+    string? num = Console.ReadLine();
+
+    if (int.TryParse(num, out int iVal))
+    {
+        Console.WriteLine($"Your number is {iVal}");
+    }
+    else if (double.TryParse(num, out double dVal))
+    {
+        Console.WriteLine($"Your number is {dVal}");
+    }
+    else if (bool.TryParse(num, out bool bVal))
+    {
+        Console.WriteLine($"Your boolean is {bVal}");
+    }
+    else
+    {
+        Console.WriteLine("Not a number");
+    }
+}
