f69782b7cd254079ecda6d445cc3c888b8ff54bd
chri-ss <christorper@gmail.com>
2023-05-11 22:14:38 -0700

    Record a score and write to/read from a file

---
+++ Chapter-39/ScoreKeeper/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Please enter your name:");
+
+string player = Console.ReadLine(); 
+int score = 0;
+
+if (File.Exists($"{player}.txt"))
+{
+    string oldScore = File.ReadAllText($"{player}.txt");
+    Console.WriteLine($"Previous score: {oldScore}");
+    score = Convert.ToInt32(oldScore);
+}
+
+while (Console.ReadKey().Key != ConsoleKey.Enter)
+{
+    score++;
+    Console.WriteLine(score);
+}
+
+File.WriteAllText($"{player}.txt", score.ToString());
